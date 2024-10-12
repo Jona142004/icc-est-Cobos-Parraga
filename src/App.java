7521aa812ecf978805c9d8f6ba16b1eb6ac49dc4
Jonnathan Parraga <jonnathanhola@gmail.com>
2024-10-12 18:48:44 -0500

    hoy

--- src/App.java
+++ src/App.java
@@ -2,6 +2,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hola soy mateo");
-        
+        Metodos ordenador = new Metodos();
+        int[] arregloBurbuja = { 34, 2, 10, 6, 7, 5, 1, 15 };
+        int[] arregloBurbujaOrdenado1 = ordenador.burbujaTradicional(arregloBurbuja);
+        System.out.println(
+        "Resultado burbuja tradicional Metodo 1 : " +
+        java.util.Arrays.toString(arregloBurbujaOrdenado1));
     }
 }
--- src/Metodos.java
+++ src/Metodos.java
@@ -1,3 +1,21 @@
+import java.util.Arrays;
+
 public class Metodos {
+    public int[] burbujaTradicional(int[] arregloOriginal) {
+        int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
+
+        int n = arreglo.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arreglo[i] > arreglo[j]) {
+                    // Intercambio de elementos
+                    int temp = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = temp;
+                }
+            }
+        }
+        return arreglo;
+    }
 
 }
