0d0556ed21a6baacacf7eebbdaf961df04810f22
mvcobos <vcobos178@gmail.com>
2024-10-12 20:32:55 -0500

    fais

--- src/App.java
+++ src/App.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hola soy mateo");
-        
+        Scanner scanner = new Scanner(System.in);
+        Interfaz interfaz = new Interfaz();
+        System.out.println("Ingrese el tamanio del arreglo");
+        int tamanio = interfaz.leerEnteroValido(scanner, "Ingrese el tamanio", false);33
+        int [] arreglo = new int[tamanio];
+        for(int k = 0; k < tamanio; k++ ){
+            arreglo[k] = interfaz.leerEnteroValido(scanner, "Ingrese el valor de la posicion " + (k+1), true);
+        } 
     }
 }
